5f6b47d18ee48cb049411f5201f2bc3caa824e81
Damien SOREL <damien.sorel@sogelink.fr>
2020-09-25 14:04:21 +0200

    Fix generate-key on Linux

--- App/android/generate-key.bat
+++ App/android/generate-key.bat
@@ -1,3 +1,6 @@
+del "%~dp0\key.jks"
+del "%~dp0\key.properties"
+
 "C:\Program Files\Android\Android Studio\jre\bin\keytool" -genkey -v -keystore "%~dp0\key.jks" -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias key -storepass 123456 -keypass 123456 -dname "CN=Mistic, O=StrangePlanet, L=Lyon, S=ARA, C=FR"
 
 (
--- App/android/generate-key.sh
+++ App/android/generate-key.sh
@@ -2,9 +2,12 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+rm -f "${DIR}/key.jks"
+rm -f "${DIR}/key.properties"
+
 keytool -genkey -v -keystore "${DIR}/key.jks" -storetype JKS -keyalg RSA -keysize 2048 -validity 10000 -alias key -storepass 123456 -keypass 123456 -dname "CN=Mistic, O=StrangePlanet, L=Lyon, S=ARA, C=FR"
 
-cat "storePassword=123456
+echo "storePassword=123456
 keyPassword=123456
 keyAlias=key
 storeFile=..\\key.jks" > "${DIR}/key.properties"
